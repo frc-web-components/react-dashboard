c44206625d4d7dd961a2ccd17e554da7b24b425f
Emily Charles <emily.w.charles@gmail.com>
2024-05-28 22:04:30 -0400

    add key to connection statuses

--- src/Titlebar.tsx
+++ src/Titlebar.tsx
@@ -180,6 +180,7 @@ function Titlebar() {
             {Object.values(connectionStatuses).map((status) => {
               return (
                 <div
+                  key={`title-item-${status.label}`}
                   style={{
                     display: "flex",
                     alignItems: "center",
