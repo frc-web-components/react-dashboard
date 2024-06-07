6f1ec8d336c12c59ea192a27ce2f4d069e007723
amorygalili <amorygalili@gmail.com>
2024-06-06 22:23:06 -0400

    Added support for displaying sources as strings inside markdown

--- src/components/MarkdownViewer.tsx
+++ src/components/MarkdownViewer.tsx
@@ -1,6 +1,8 @@
 import MDEditor from "@uiw/react-md-editor";
 import styles from "./MarkdownViewer.module.scss";
 import { createComponent, markdownProp } from "./fromProps";
+import { getAssetUrl } from "../main";
+import { ntMarkdownComponent } from "../tools/properties/MarkdownEditor";
 
 export const markdownViewer = createComponent(
   {
@@ -10,8 +12,8 @@ export const markdownViewer = createComponent(
       defaultSize: { width: 200, height: 50 },
       minSize: { width: 50, height: 50 },
     },
-    primaryProperty: 'markdown',
-    acceptedSourceTypes: ['String'],
+    primaryProperty: "markdown",
+    acceptedSourceTypes: ["String"],
     properties: {
       markdown: markdownProp({ defaultValue: "## Hello there" }),
     },
@@ -22,6 +24,13 @@ export const markdownViewer = createComponent(
         <MDEditor.Markdown
           source={markdown}
           style={{ background: "none" }}
+          components={{
+            img: ({ src, ...props }) => {
+              const srcUpdated = !src?.startsWith("/") ? src : getAssetUrl(src);
+              return <img {...props} src={srcUpdated} />;
+            },
+            ["nt" as any]: ntMarkdownComponent,
+          }}
         />
       </div>
     );
--- src/tools/properties/MarkdownEditor.tsx
+++ src/tools/properties/MarkdownEditor.tsx
@@ -1,8 +1,59 @@
 import { CustomCellEditorProps } from "ag-grid-react";
-import { memo } from "react";
+import { memo, useMemo } from "react";
 import MDEditor, { ICommand, divider, getCommands } from "@uiw/react-md-editor";
 import { PropertyData } from "./Properties";
 import { getAssetUrl } from "../../main";
+import { selectSourceValue } from "../../store/selectors/sourceSelectors";
+import { useAppSelector } from "../../store/app/hooks";
+
+interface NTProps {
+  source?: string;
+  defaultValue?: string;
+  additionalProps?: any[];
+}
+
+export function NT({
+  source: sourceString,
+  defaultValue,
+  additionalProps,
+}: NTProps) {
+  const source = useMemo(() => {
+    if (!sourceString) {
+      return undefined;
+    }
+    if (sourceString.includes(":")) {
+      const [key, provider] = sourceString.split(":");
+      return { key, provider };
+    }
+    return { provider: "NT", key: sourceString };
+  }, [sourceString]);
+
+  const value = useAppSelector((state) =>
+    selectSourceValue(state, source?.provider, source?.key)
+  );
+
+  return (
+    <span {...additionalProps}>
+      {typeof value !== "undefined"
+        ? JSON.stringify(value)
+        : defaultValue ?? ""}
+    </span>
+  );
+}
+
+export const ntMarkdownComponent = ({
+  source,
+  default: defaultValue,
+  children,
+  ...props
+}: any) => {
+  return (
+    <>
+      <NT source={source} defaultValue={defaultValue} additionalProps={props} />
+      {children}
+    </>
+  );
+};
 
 export default memo((props: CustomCellEditorProps<PropertyData>) => {
   const { value, onValueChange, stopEditing } = props;
@@ -54,11 +105,10 @@ export default memo((props: CustomCellEditorProps<PropertyData>) => {
         previewOptions={{
           components: {
             img: ({ src, ...props }) => {
-              const srcUpdated = !src?.startsWith('/') ? src : getAssetUrl(src);
-              return (
-                <img {...props} src={srcUpdated} />
-              );
+              const srcUpdated = !src?.startsWith("/") ? src : getAssetUrl(src);
+              return <img {...props} src={srcUpdated} />;
             },
+            ["nt" as any]: ntMarkdownComponent,
           },
         }}
       />
