923dbb70e9ae01823836b5a79ff79354233e783a
Emily Charles <emily.w.charles@gmail.com>
2024-12-24 20:06:33 -0500

    one more typo

--- packages/dashboard/src/store/slices/appSlice.ts
+++ packages/dashboard/src/store/slices/appSlice.ts
@@ -42,5 +42,5 @@ export const { toggleEditing, setContextMenuElement, setEditing } =
   appSlice.actions;
 export const {
   selectEditing,
-  selectContextMenuElement: selectConextMenuElement,
+  selectContextMenuElement: selectContextMenuElement,
 } = appSlice.selectors;
